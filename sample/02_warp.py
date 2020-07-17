9d338faec056a36160f9d6c062dae44841637913
Shingo W. Kagami <swk@kagami.org>
2020-07-17 18:00:34 +0900

    Add a sample implementation of warpPerspective().

--- sample/02_warp.py
+++ sample/02_warp.py
@@ -18,6 +18,41 @@ dest_pnts = np.array([[0, 0],
                       [0, dest_size - 1]], 
                      np.float32)
 
+@jit
+def warpPerspective_impl(src, H, dest_shape):
+    width = dest_shape[1]
+    height = dest_shape[0]
+    swidth = src.shape[1]
+    sheight = src.shape[0]
+    dest = np.empty((dest_shape[0], dest_shape[1], 3), dtype=np.uint8)
+    Hinv = np.linalg.inv(H)
+    for j in range(height):
+        x0 = Hinv[0, 1] * j + Hinv[0, 2]
+        y0 = Hinv[1, 1] * j + Hinv[1, 2]
+        w0 = Hinv[2, 1] * j + Hinv[2, 2]
+        for i in range(width):
+            x = Hinv[0, 0] * i + x0;
+            y = Hinv[1, 0] * i + y0;
+            w = Hinv[2, 0] * i + w0;
+            winv = 1.0 / w if w != 0.0 else 0.0
+            x *= winv
+            y *= winv
+            x = min(swidth - 2.0, max(0.0, x))
+            y = min(sheight - 2.0, max(0.0, y))
+            x_int = int(x)
+            x_frac = x - x_int
+            y_int = int(y)
+            y_frac = y - y_int
+            weight = np.array([(1 - x_frac) * (1 - y_frac), 
+                               x_frac * (1 - y_frac), 
+                               (1 - x_frac) * y_frac, 
+                               x_frac * y_frac])
+            dest[j, i] = (weight[0] * src[y_int, x_int] 
+                          + weight[1] * src[y_int, x_int + 1]
+                          + weight[2] * src[y_int + 1, x_int]
+                          + weight[3] * src[y_int + 1, x_int + 1])
+    return dest
+
 def onMouse(event, x, y, flag, param):
     global pnt_selected, src_pnts
 
@@ -46,7 +81,8 @@ if __name__ == '__main__':
     while True:
 
         H = cv2.getPerspectiveTransform(src_pnts, dest_pnts)
-        output = cv2.warpPerspective(input, H, (dest_size, dest_size))
+        #output = cv2.warpPerspective(input, H, (dest_size, dest_size))
+        output = warpPerspective_impl(input, H, (dest_size, dest_size))
 
         disp = input.copy()
         for k in range(4):
